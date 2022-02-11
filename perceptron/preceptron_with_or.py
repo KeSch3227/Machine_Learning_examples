79195f5ade9afcb9a4e63612ca8fd351878a0336
Kevin Lührs <kevin.luehrs@bystronic.com>
2022-02-11 15:25:29 +0100

    created perceptron class

---
+++ perceptron/preceptron_with_or.py
@@ -0,0 +1,31 @@
+from typing import Tuple
+import numpy as np
+
+def get_dataset() -> Tuple[np.ndarray, np.ndarray]:
+    """OR dataset."""
+    x=np.array([[0,0][1,0][0,1][1,1]])
+    y= np.array([[0],[1],[1],[1]])
+    return x,y
+
+def accuracy_score(y_true:np.ndarray,y_pred: np.ndarray) -> float:
+    pass
+
+def step_function(input_signal :np.ndarray) -> np.ndarray:
+    pass
+
+class Proceptron:
+    
+    def __init__(self) ->None:
+        pass
+
+    def _update_weights(self, x:np.ndarray, y_pred:np.ndarray)-> None:
+        pass
+
+    def train(self, x:np.ndarray, y_pred:np.ndarray, epochs:int = 1) -> None:
+        pass
+
+    def predict(self, x:np.ndarray) -> np.ndarray:
+        pass
+
+    def evaluate(self,x:np.ndarray,y:np.ndarray)-> None :
+        pass 
